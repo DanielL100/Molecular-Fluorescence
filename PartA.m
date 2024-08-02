9cd85129a52beb3220ba24e7b40be249f9545d93
DanielL100 <49718952+DanielL100@users.noreply.github.com>
2024-08-02 10:29:11 +0300

    Add files via upload

--- PartA.m
+++ PartA.m
@@ -1,4 +1,4 @@
-%% log(Intensity) as a function of wavenumber for each Material
+%% log(Intensity) as a function of wavelength for each Material
 
 % Closing all open figures and clearing command line
 close all
